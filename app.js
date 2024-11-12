bedf378b52692c791e8e3b76619bdffa8c1bbec0
dzjose <86900854+dzjose@users.noreply.github.com>
2024-11-12 15:13:27 -0800

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+// Card Flip
+document.querySelectorAll('.services__card').forEach(card => {
+  card.addEventListener('click', () => {
+    card.querySelector('.card-content').classList.toggle('flip');
+  });
+});
